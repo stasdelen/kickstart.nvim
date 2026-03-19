429b4dfe0c294f2489cd72d24a12ddf593c3166f
Salih TAŞDELEN <stasdelen@barracuda.local>
2026-03-19 21:44:33 +0300

    Update neo-tree filters

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -19,6 +19,11 @@ return {
   ---@type neotree.Config
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
