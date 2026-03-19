ae46bf1cae21d66859006be91866912448fec268
Salih TAŞDELEN <stasdelen@barracuda.local>
2026-03-19 21:44:16 +0300

    Add neoclip custom plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,4 +5,26 @@
 
 ---@module 'lazy'
 ---@type LazySpec
-return {}
+return {
+  'AckslD/nvim-neoclip.lua',
+  dependencies = {
+    { 'kkharji/sqlite.lua', module = 'sqlite' },
+    { 'nvim-telescope/telescope.nvim' },
+  },
+  config = function()
+    require('neoclip').setup {
+      -- Optional: keep unnamed + system clipboard in sync via neoclip
+      default_register = { '"', '+' },
+
+      on_select = {
+        move_to_front = true,
+        close_telescope = true,
+      },
+      on_paste = {
+        set_reg = true, -- <== update registers when using paste keys
+        move_to_front = true,
+        close_telescope = true,
+      },
+    }
+  end,
+}
